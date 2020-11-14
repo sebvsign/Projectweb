f01b43c39c8fcfc5e7a51187cc71bc8ef069a832
Pablo gacitua <pabl.gacitua@alumnos.duoc.cl>
2020-11-14 00:34:07 -0300

    Agregando prueba unitaria

--- ProyectowebApp/tests.py
+++ ProyectowebApp/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
-
+from django.urls import reverse, resolve
+from .models import Usuario
 # Create your tests here.
+
+class testUrls():
+
+    def Test_detail_url(self):
+        path = reverse('formulario', kwargs={'pk': 1})
+        assert resolve(path).view_name == 'formulario'
+
