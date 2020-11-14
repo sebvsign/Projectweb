83006764c28f83854457f12a97e47a51affe6e71
SPC <s.painen@alumnos.duoc.cl>
2020-11-14 12:51:48 -0300

    Eliminando maqueta de usuario y teams

--- ProyectowebApp/admin.py
+++ ProyectowebApp/admin.py
@@ -2,17 +2,3 @@ from django.contrib import admin
 from .models import Usuario, Regiones, Teams
 # Register your models here.
 
-class UsuarioAdmin(admin.ModelAdmin):
-    list_display = ['Nickname', 'Correo', 'asunto', 'Comentario']
-    search_fields = ['Nickname', 'Correo']
-    list_filter =['Nickname']
-
-class TeamsAdmin(admin.ModelAdmin):
-    list_display = ['nombre','etiqueta','logo','region']
-    search_fields = ['nombre','region']
-    list_filter =['region']
-
-admin.site.register(Usuario,UsuarioAdmin)
-
-admin.site.register(Regiones)
-admin.site.register(Teams)
